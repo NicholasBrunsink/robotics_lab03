0e43e6527fbfe0066fc13ec31c243e08bc088597
NicholasBrunsink <nicholasbrunsink@gmail.com>
2023-02-20 21:49:07 -0500

    Finialized Code

--- scripts/robot_config.py
+++ scripts/robot_config.py
@@ -1,12 +1,26 @@
+# import solution for problem 1
 import robot_model
+# import numpy and math for matrix and math functionality
 import numpy as np
 import math as m
 
 np.set_printoptions(precision = 2, suppress = True)
 
 def moveBot(params):
+    '''
+    Calculates the position and rotation of an n-DOF robot 
+    as defined by Denavit-Hartenberg parameters inputted.
+    Parameter:
+        params - list of parameters for a given robot, contains theta, a, alpha, and d for each frame.
+    Returns:
+        None 
+    '''
+    # Calculating DH matrix using function from problem 1
     dhMatrix = robot_model.kinematic_chain(params)
+
+    # Extracting the position information from the DH matrix using function form problem 1
     pos = robot_model.get_pos(dhMatrix)
+    # Extracting the rotation information from the DH matrix using function form problem 1
     rot = robot_model.get_rot(dhMatrix)
 
     print(f'x = {pos[0]:.2f} \tmeters\ny = {pos[1]:.2f} \tmeters\nz = {pos[2]:.2f} \tmeters')
@@ -14,11 +28,15 @@ def moveBot(params):
 
 if __name__ == "__main__":
 
+    # Defining parameters for problem A
+    # 2 DOF robot arm defined in lectures
     paramsA = np.array([
         [m.pi/2, 1, 0, 0],
         [m.pi/2, 1, 0, 0]
     ])
 
+    # Defining parameters for problem B part 1
+    # 6 DOF robot arm defined in lectures
     paramsB1 = np.array([
         [0,     0,         0.1625,     m.pi/2],
         [0,    -0.425,     0,          0],
@@ -28,6 +46,8 @@ if __name__ == "__main__":
         [0,     0,         0.0996,     0]
     ])
 
+    # Defining parameters for problem B part 2
+    # 6 DOF robot arm defined in lectures
     paramsB2 = np.array([
         [0,         0,         0.1625,     m.pi/2],
         [-m.pi/2,  -0.425,     0,          0],
--- scripts/robot_model.py
+++ scripts/robot_model.py
@@ -2,6 +2,22 @@ import math as m
 import numpy as np
 
 def dh_transformation(theta, a, d, alpha):
+    '''
+    Generate a homogenous transformation matrix based on a single frame
+    from iputted Denavit-Hartenberg parameters using the matrix below.
+    |  cos(theta), -sin(theta)*cos(alpha),   sin(theta)*m.sin(alpha),   a*cos(theta)  |
+    |  sin(theta),  cos(theta)*cos(alpha),  -cos(theta)*m.sin(alpha),   a*sin(theta)  |
+    |  0,           sin(alpha),              cos(alpha),                d             |
+    |  0,           0,                       0,                         1             |
+    
+    Parameters:
+        theta - angle to be rotated
+        a - link length
+        alpha - twist angle between z axis of current frame and previous frame.
+        d - link offset from previous link
+    Returns:
+        a homogenous transformation matrix based on the parameters.
+    '''
     return np.array(
         [[m.cos(theta), -m.sin(theta)*m.cos(alpha),   m.sin(theta)*m.sin(alpha),   a*m.cos(theta)],
          [m.sin(theta),  m.cos(theta)*m.cos(alpha),  -m.cos(theta)*m.sin(alpha),   a*m.sin(theta)],
@@ -9,6 +25,15 @@ def dh_transformation(theta, a, d, alpha):
          [0,             0,                           0,                           1]])
 
 def kinematic_chain(dhParams):
+    '''
+    Calculates the homogenous transformation matrix for each frame
+    Parameters:
+        dhParams - a list containing the Denavit-Hartenberg parameters 
+            for each frame of a robot.
+    Returns:
+        TMatrix - the final transformation computed from every frame
+    '''
+    # Initializing a 4x4 identity matrix to build off of
     TMatrix = np.array([
         [1,0,0,0],
         [0,1,0,0],
@@ -16,7 +41,9 @@ def kinematic_chain(dhParams):
         [0,0,0,1]]
     )
 
+    # Iterating through each frame in dhParams
     for joint in range(len(dhParams)):
+        # Calculate the frame's tranformation matrix based on dhParams and the previous TMatrix using the current frame.
         TMatrix = np.matmul(TMatrix, dh_transformation(dhParams[joint][0],
                                                         dhParams[joint][1], 
                                                         dhParams[joint][2], 
@@ -24,11 +51,16 @@ def kinematic_chain(dhParams):
     return TMatrix
 
 def get_pos(H):
+    '''Returns the x, y, and z coordinates from an inputted 4x4 Homogenous transformation matrix'''
     return np.array([H[0][3], H[1][3], H[2][3]])
 
 def get_rot(H):
+    '''Returns the roll, pitch, and yaw rotation calculated from an inputted 4x4 Homogenous transformation matrix'''
+    # Calculate roll
     roll = m.atan2(H[2][1], H[2][2])
+    # Calculate pitch
     pitch = m.atan2(-H[2][0], m.sqrt(H[2][1]**2 + H[2][2]**2))
+    # Calculate yaw
     yaw = m.atan2(H[1][0], H[0][0])
 
     return np.array([roll, pitch, yaw])
